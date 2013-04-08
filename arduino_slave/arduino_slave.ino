a0199a2a46eead88885078e57ee743d0b82b6709
Nate McBean <nmcbean@gmail.com>
2013-04-07 21:51:22 -0400

    Added Trigger Button
    New! Trigger button on slave arduino (well will be on master as well
    eventually). Sends an automatic serial message. "Debounced" to trigger a
    max of once a second.

--- arduino_slave/arduino_slave.ino
+++ arduino_slave/arduino_slave.ino
@@ -29,6 +29,7 @@
 #define OLED_MOSI   9
 #define OLED_RESET  13
 #define SPEAKER_PIN 6
+#define TRIGGER_PIN 2
 
 /*** global creations ***/
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
@@ -47,15 +48,24 @@ String console_input = "";
 boolean consoleComplete = false;
 int health;
 
+volatile int trigger_button_pushed = 0;
+unsigned long last_trigger;
+
 
 /*** initialization and setup ***/
 void setup()   {                
-  
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  /**********************************************************************************/
+  /* setup display */
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
-  
-  // initialize serial:
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Powering up...");
+  display.display();
+
+  /**********************************************************************************/  
+  /* setup serial */
   Serial1.begin(19200);
   Serial.begin(9600);
   //while (!Serial) ;
@@ -67,22 +77,37 @@ void setup()   {
   console_input.reserve(200);
   
   last_screen_update = 0;
-  health = 100;
+  health = 100; 
+
+  /**********************************************************************************/
+  /* send a dummy message to the imp */
+  Serial1.println("Auto comm from slave imp...,");
   
-  // init done
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Ello");
-  display.display();
-  
-  Serial1.println("Testing...,");
+  /**********************************************************************************/
+  /* setup trigger button */
+  trigger_button_pushed = 0;
+  pinMode(TRIGGER_PIN, OUTPUT);
+  // this needs to be 0 because the first (0th) interrupt is on pin 2
+  // THE STUPID LEONARDO MESSED UP NUMBERING- pin 3 = int 0
+  // use PIN3 on the leonardo. use PIN2 on the pro ugh.
+  attachInterrupt(0,trigger_push, RISING);
+  last_trigger =0;  
+}
+
+// we hit the trigger! don't do serial operations in here. get out ASAP
+// (from the documentation "Inside the attached function, delay() won't work and the 
+// value returned by millis() will not increment. Serial data received while in the 
+// function may be lost. You should declare as volatile any variables that you modify 
+// within the attached function.
+
+void trigger_push () {
+  trigger_button_pushed = 1; 
 }
 
 /*** never ending main loop ***/
 void loop() {
-  //beep();
   
+  // read from the console
   if(Serial.available() > 0)
   {
     char inChar = (char)Serial.read();
@@ -97,9 +122,10 @@ void loop() {
     }
   }
   
+  // if finished reading from teh console, send it to the imp (and print to console)
   if (consoleComplete) {
     // print to the console
-    Serial.print("sent: ");
+    Serial.print("tx to imp: ");
     Serial.println(console_input);
     
     // send to the imp
@@ -109,6 +135,7 @@ void loop() {
     console_input = "";
   }
   
+  // read fromt he imp
   if(Serial1.available() > 0)
   {
     // get the new byte:
@@ -125,6 +152,7 @@ void loop() {
     
   }
 
+  // if finished reading from the imp, send it to the console
   // print the string when a newline arrives:
   if (stringComplete) {
     health = 100;
@@ -132,6 +160,7 @@ void loop() {
     updateScreen();
     
     // maintenance for the next loop
+    Serial.print("rx from imp: ");
     Serial.println(inputString); 
     
     // clear the string:
@@ -141,11 +170,26 @@ void loop() {
     beep();
   }
   
+  // update the screen every second
   if(millis() > last_screen_update + 1000) {
     if (health >= 5) {
       health -= 5; 
     }
     updateScreen();
+  }
+  
+  // handle the button presses here
+  if( trigger_button_pushed && millis() > last_trigger + 1000) {
+    trigger_button_pushed = 0;
+    Serial.println("Sending trigger!");
+    // temporarily keep using the comma delimiter
+    // soon will just send the one char
+    Serial1.print("s,");
+    last_trigger = millis();
+  } else if ( trigger_button_pushed ) {
+    // oh no! we bounced!
+    // kill the spare
+    trigger_button_pushed = 0; 
   }
 }
 
